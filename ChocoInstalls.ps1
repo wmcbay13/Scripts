32f762ca229d32b391788209541bb95b7fcab3dd
wmcbay13 <68028014+wmcbay13@users.noreply.github.com>
2020-08-12 10:44:39 -0400

    Choco package initial commit

---
+++ ChocoInstalls.ps1
@@ -0,0 +1,20 @@
+<# Scripted installations by Chocolatey:
+    Google Chrome
+    TreeSize Free
+    7zip
+    NotepadPlusPlus
+    VS Code
+    Jave Runtime 8
+    .NET Framework 4.8
+
+    Package Repository 08/10/2020 by ~Will McBay
+#>
+
+choco install googlechrome -y
+choco install treesizefree -y
+choco install 7zip.install -y
+choco install notepadplusplus.install -y
+choco install vscode -y
+choco install jre8 -y
+choco install netfx-4.8-devpack -y
+
---
+++ Tenant-ELK.ps1
@@ -0,0 +1,108 @@
+#Configure ELK logs
+$elkserver = $ELKServer
+$Region = $ELKRegion
+$service = "tenant"
+$environment = $ELKEnvironment
+$LogFolder = $ELKTenantLogs
+$InstallFolder = $ELKInstallPath
+
+Write-Output "Server: $ElkServer"
+Write-Output "Product: $ELKProduct"
+Write-Output "service: $service"
+Write-Output "LogFolder: $LogFolder"
+Write-Output "environment: $environment"
+Write-Output "InstallFolder: $InstallFolder"
+
+Function UpdateYML {
+    param($File, $ELK, $Region, $Service, $Env, $Logs)
+    Write-Host "Updating YML file $File"
+    $NewYML = @()
+    $Skip = $false
+    $SetupILM = $false
+    $Inputs = $false
+    $Paths = $false
+    $Fields = $false
+    $SetupTemplate = $false
+    $ElasticSearch = $false
+    $LogStash = $false
+    $CertFolder = "C:/ELK/certs"
+    Get-Content $File | %{
+        $Skip = $false
+        if($_ -match 'setup.ilm'){$SetupILM = $true}
+        if($_ -match 'inputs:' -and $SetupILM -eq $false){$NewYML += "setup.ilm.enabled: false";$inputs=$true}
+        if($_ -match 'enabled:' -and $inputs ){$NewYML += "  enabled: true";$Skip = $true;$inputs = $false}
+        if($_ -match 'metricbeat.config.modules:' -and $SetupILM -eq $false){$NewYML += "setup.ilm.enabled: false";$SetupILM= $true}
+        if($_ -match 'paths:'){$Paths = $true}
+        if($_ -match '    - ' -and $Paths){$Paths = $false;$Skip = $true;$NewYML += "    - $Logs\*.txt"  }
+        if($_ -match 'setup.template'){$SetupTemplate = $true}
+        if($_ -match 'fields:' -and $SetupTemplate){$Skip = $true;$Fields = $true;$NewYML += "fields:";$NewYML += "  env: $Env";$NewYML += "  region: $Region";$NewYML += "  service: $Service";}
+        #if($_ -match 'fields:' -and $SetupTemplate){$Skip = $true;$Fields = $true;$NewYML += "fields:";$NewYML += "  env: $Env";$NewYML += "  region: $Region";$NewYML += "  service: api";}
+        #if($_ -match 'fields:' -and $SetupTemplate){$Skip = $true;$Fields = $true;$NewYML += "fields:";$NewYML += "  env: $Env";$NewYML += "  region: $Region";$NewYML += "  service: tenant";}
+        if($_ -match '  ' -and $Fields){$Skip = $true}
+        if($_ -match '#' -and $Fields){$Skip = $false;$Fields = $false}
+        if($_ -match 'setup.kibana:'){$NewYML += "#setup.kibana:";$Skip = $true}
+        if($_ -match 'output.elasticsearch:'){$ElasticSearch = $true; $NewYML += "#output.elasticsearch:";$Skip = $true}
+        if($_ -match 'hosts:' -and $ElasticSearch){$ElasticSearch = $false;$NewYML += "#" + $_ ;$Skip = $true}
+        if($_ -match 'output.logstash'){$LogStash = $true; $NewYML += "output.logstash:";$Skip = $true}
+        if($_ -match 'hosts' -and $LogStash){$LogStash = $false;$NewYML += '  hosts: ["' + "$($ELK):443" + '"' + "]";$Skip = $true}
+        if($_ -match 'ssl.certificate_authorities'){$NewYML += '  ssl.certificate_authorities: ["' + "$($CertFolder)/fortifyopsca.pem" + '"' + "]";$Skip = $true}
+        if($_ -match 'ssl.certificate:'){$NewYML += '  ssl.certificate: "' + "$($CertFolder)/filebeat.pem" + '"' ;$Skip = $true}
+        if($_ -match 'ssl.key'){$NewYML += '  ssl.key: "' + "$($CertFolder)/filebeat.key" + '"' ;$Skip = $true}
+        if(-Not $Skip){
+            $NewYML += $_ 
+        }
+	}
+    return $NewYML   
+}
+
+ForEach ($BeatFolder in (gci $InstallFolder -directory | ?{$_ -match 'beat'})){
+    Write-Output "Current Folder: $($BeatFolder.FullName)"
+    $filename = $BeatFolder.Name.split('-')[0]
+    Write-Output "Filename: $filename"
+    #if($filename -notin $Packages){echo "$filename is not Included in deployment, update ELKBeatPackages to include";continue}
+    $file = Join-Path $BeatFolder.FullName "$($filename).yml"
+    Write-Output "File: $file"
+    $NewYML = @()
+    $Skip = $false
+    $LogStashM =  $false
+    switch($filename){
+        'filebeat' {
+            $FileYML = UpdateYML $file $elkserver $Region $service $environment $LogFolder
+            $FileYML | %{ 
+                $NewYML += $_ 
+            }
+            break
+        }
+        'metricbeat' {
+            $FileYML = UpdateYML $file $elkserver $Region $service $environment $LogFolder
+            $FileYML | %{ 
+                $Skip = $false
+                if($_ -match 'output.logstash'){$LogStashM = $true; $NewYML += "output.logstash:";$Skip = $true}
+                if($_ -match '  hosts:' -and $LogStashM ){$LogStashM = $false;$NewYML += '  hosts: ["' + "$($elkserver):8443" + '"' + "]";$Skip = $true; Write-Output "FOUND HOSTS"}
+                if(-Not $Skip){
+                    $NewYML += $_ 
+                }
+            }
+            break
+        }
+        'winlogbeat' {
+            $FileYML | %{ 
+                $NewYML += $_ 
+            }
+            break
+        }
+        default {
+            $FileYML | %{ 
+                $NewYML += $_ 
+            }
+        }
+    }
+    Write-Output "NewYML = $($NewYML.count) lines"
+    $NewYML | set-content $file
+    
+}
+
+
+
+
+
